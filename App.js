b3926d7213f6c5665fb3a7cbc7af0a30b0943c4b
fulanodetalex <fulanodetalex@gmail.com>
2023-05-11 21:02:45 -0500

    clase 11-05-2023

--- App.js
+++ App.js
@@ -1,15 +1,111 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import {useForm, Controller} from 'react-hook-form'
+import {TextInput, Button} from 'react-native-paper'
+
 
 export default function App() {
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    defaultValues: {
+      firstName: '',
+      lastName: ''
+    }
+  });
+  const onSubmit = data => console.log(data);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Controller
+        control={control}
+        rules={{
+         required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            label="Nombre completo"
+            mode="outlined"
+            left={<TextInput.Icon icon="account"/>}
+            style={{marginBottom: 20, backgroundColor:'powderblue'}}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="firstName"
+      />
+      {errors.firstName && <Text style={{color:'red'}}>El nombre es obligatorio</Text>}
+
+      <Controller
+        control={control}
+        rules={{
+         maxLength: 100,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+          label="Apellidos"
+          mode="outlined"
+          left={<TextInput.Icon icon="account"/>}
+          style={{marginBottom: 20, backgroundColor:'powderblue'}}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="lastName"
+      />
+      {errors.firstName && <Text style={{color:'red'}}>El apellido es obligatorio</Text>}
+   <View style={{flexDirection:'row'}}>
+      <Button 
+      icon="plus-box" 
+      mode="contained" 
+      onPress={() => console.log('Pressed')}
+      style={{backgroundColor:'green'}}
+      >
+          Guardar
+     </Button>
+     <Button 
+      icon="card-search-outline" 
+      mode="contained" 
+      onPress={() => console.log('Pressed')}
+      style={{backgroundColor:'blue'}}
+      >
+          Buscar
+     </Button>
+  </View>
+  
+  <View style={{flexDirection:'row', marginTop:20}}>
+      <Button 
+      icon="pencil-outline" 
+      mode="contained" 
+      onPress={() => console.log('Pressed')}
+      style={{backgroundColor:'green'}}
+      >
+          Editar
+     </Button>
+     <Button 
+      icon="delete-outline" 
+      mode="contained" 
+      onPress={() => console.log('Pressed')}
+      style={{backgroundColor:'red'}}
+      >
+          eliminar
+     </Button>
+  </View>
+
+  <View style={{flexDirection:'row', marginTop:20}}>
+      <Button 
+      icon="view-list" 
+      mode="contained" 
+      onPress={() => console.log('Pressed')}
+      style={{backgroundColor:'purple'}}
+      >
+          Listar
+     </Button>
+  </View>
+</View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
